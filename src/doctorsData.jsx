94715b76636486fe521f2d850252b57566e34ffd
efs0cod3 <eflearnng@gmail.com>
2026-03-30 10:11:54 -0400

    changes to images routes in doctorsDATA

--- src/doctorsData.jsx
+++ src/doctorsData.jsx
@@ -1,12 +1,12 @@
 export default [
       {
         id: 1,
-        img: "src/assets/equipo/nin.jpg",
+        img: "/assets/equipo/nin.jpg",
         name: "Dr. Francisco Nin"
       },
       {
         id: 2,
-        img: "src/assets/equipo/yesica-sent.jpeg",
+        img: "/assets/equipo/yesica-sent.jpeg",
         name: "Dr. Yessica Palmy"
       },
 ]
